4a7b6a9906d5a9877b5f7499a6c66cd639997754
lexicalunit <lexicalunit@lexicalunit.com>
2024-04-03 12:48:31 -0700

    Install with yarn

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 yarn install
 mkdir -p /opt/busylight
-cp -r node_modules cli.js /opt/busylight
+cp -r node_modules cli.js package.json yarn.lock /opt/busylight
 test -e busylight.json && cp "$_" /opt/busylight
